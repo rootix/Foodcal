99ed4fcc5c428a3d726c04915ed14740bc24597a
Pascal Mathys <pascal.mathys@rootix.ch>
2023-07-06 23:29:43 +0200

    Prepare production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
     production: true,
+    supabaseUrl: 'https://fflqtkbytjhczpurtqdq.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmbHF0a2J5dGpoY3pwdXJ0cWRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgzMjIwMTksImV4cCI6MjAwMzg5ODAxOX0.mYDGo2eWSepN489vYfe8bnxwurNfq1TLqagDFo__EwI',
 };
